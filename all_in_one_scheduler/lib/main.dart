3dfa57c4278ca5faf077b312ac1efda7591a95bb
dbstjr3067 <dbstjr3067@gmail.com>
2025-11-05 23:35:45 +0900

    BottomNavigationbar(하단 바) 추가

--- all_in_one_scheduler/lib/main.dart
+++ all_in_one_scheduler/lib/main.dart
@@ -10,10 +10,117 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
 //이 아래로 코드 작성, 위는 무시
     return MaterialApp(
-      home: Center(
-        child: Container(width:50, height:50, color: Colors.blue),
-      )
+      debugShowCheckedModeBanner: false,
+      title: '올인원 스케쥴러',
+      home: const HomePage(),
     ); //MaterialApp
-    //dasdlkdnlakdnlsdfnaslkfndlkn
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = const [
+    ReminderScreen(),
+    SchedulerScreen(),
+    StatisticsScreen(),
+    AlarmScreen()
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index; //탭 클릭 시 인덱스 변경
+    });
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed, // 4개 이상이면 이게 필요
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.attach_file),
+            label: '오늘 할 일',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            label: '계획',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bar_chart),
+            label: '통계',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.alarm),
+            label: '알람',
+          ),
+        ]
+      ),
+    );
+  }
+}
+// -------------------- 화면 별 위젯 --------------------
+class ReminderScreen extends StatelessWidget {
+  const ReminderScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        '오늘 할 일 페이지',
+        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
+
+class SchedulerScreen extends StatelessWidget {
+  const SchedulerScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        '계획 세우기 페이지',
+        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
+class StatisticsScreen extends StatelessWidget {
+  const StatisticsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        '통계 페이지',
+        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
+class AlarmScreen extends StatelessWidget {
+  const AlarmScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        '알람 페이지',
+        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      ),
+    );
   }
 }
